fe64eb4737609eb19c3912b0b27d43c9fe1b182d
liuy023 <liuyminh@gmail.com>
2024-05-28 01:29:39 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/e/1FAIpQLSfvxuq5ZOhP9JEe-oWLrlJ2AYdaTrBPOMto86O6gW4EwOeZkQ/viewform?fbclid=IwZXh0bgNhZW0CMTAAAR008T0mnAY4yfHZEWlfnN5JOV9_td5MAzhar9OKB3JUyJyHkxwXr0GoOOo_aem_ATx6bK3TdaxRS-tHS65TjIQLXwPb2652UbhHvyS2xQsbwMnr31zHPU28afZKo6-jub5ljJjmZoPj49urzxw9KaY0',
+	'https://docs.google.com/forms/d/e/1FAIpQLSd3gnqlfjskwiEdZGNYIbMEMWoY-TnyROfjokR59Jwh7Mj73A/viewform?fbclid=IwZXh0bgNhZW0CMTAAAR079TTOIIMQU-JqTb8DXUjuMRFxctDoFBP2-9PIEh2LICpgG0OnnvQBkaM_aem_ATyWKv8qhMdRD-eGShwaPOwqmETeSuq-8lBOKuR1ztyaYQVunFu3AcqAeeCx-AsrkQ4Csn-qezwpEbnkqOPPqyKL'
 ];
